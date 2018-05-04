4310dd29fbf6be51fadcf1e55e55e8f1d0ee4c75
Fey Ijaware <Fey.ijaware@talent-works.com>
2018-05-04 14:01:19 +0100

    updates

--- Learning/index.js
+++ Learning/index.js
@@ -9,3 +9,22 @@ for (var i = 0, headings = $$('h1,h2,h3,h4,h5,h6');
 }
 
 //The benefits of Offline first
+
+//you register for a service worker like this, ginving the location of your service worker
+navigator.serviceWorker.register('/sw.js').then(function (reg) {
+	// body...
+	console.log('yay!');
+}).catch(function (err) {
+	// body...
+	console.log('boo!');
+});
+
+//you can also prrovide a scope and the service worker will control any page whose URL begins with the scope
+navigator.serviceWorker.register('/sw.js', {
+	scope: '/sample-app-name/'
+});
+
+//to use service worker in a safe progressive enhancing way, make sure to write your code in a simple feature detect
+if (navigator.serviceWorker) {
+	navigator.serviceWorker.register('/sw.js');
+}
