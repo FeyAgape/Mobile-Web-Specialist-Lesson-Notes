6512891dd76f21a9b22ca4fd257f0c18ac12490e
TALENTWORKS\Fey.ijaware <fshola4real@gmail.com>
2018-06-28 11:58:17 +0100

    adding in some react tutorial

---
+++ React/ex4.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyName extends React.Component {
+	// name property goes here:
+get name() {
+  return 'Fey';
+}
+  render () {   
+     return <h1>My name is {this.name}.</h1>;
+  }
+}
+
+ReactDOM.render(<MyName />, document.getElementById('app'));
