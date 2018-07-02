6cd445bd4d63a3e3184e25b05f04a8852a784d71
TALENTWORKS\Fey.ijaware <fshola4real@gmail.com>
2018-07-02 11:44:03 +0100

    adding in some react tutorial

--- React/React2/propType2.js
+++ React/React2/propType2.js
@@ -1,27 +1,31 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { BestSeller } from './BestSeller';
 
-export class BestSeller extends React.Component {
+export class BookList extends React.Component {
   render() {
     return (
-      <li>
-        Title: <span>
-          {this.props.title}
-        </span><br />
-        
-        Author: <span>
-          {this.props.author}
-        </span><br />
-        
-        Weeks: <span>
-          {this.props.weeksOnList}
-        </span>
-      </li>
+      <div>
+        <h1>Best Sellers</h1>
+        <div>
+          <ol>
+            <BestSeller 
+              title="Glory and War Stuff for Dads" 
+              author="Sir Eldrich Van Hoorsgaard" 
+              weeksOnList={10} />
+            <BestSeller 
+              title="The Crime Criminals!" 
+              author="Brenda Sqrentun" 
+              weeksOnList={2} />
+            <BestSeller
+              title="Subprime Lending For Punk Rockers" 
+              author="Malcolm McLaren" 
+              weeksOnList={600} />
+          </ol>
+        </div>
+      </div>
     );
   }
 }
 
-BestSeller.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  author: React.PropTypes.string.isRequired,
-  weeksOnList: React.PropTypes.number.isRequired
-};
+ReactDOM.render(<BookList />, document.getElementById('app'));
